34fd93a4952c557a2a9af45bced98e3b8dc815a9
Devayanee Gupta <133389582+dg-2805@users.noreply.github.com>
2024-11-13 23:15:27 +0530

    Time: 64 ms (29.34%), Space: 51 MB (93.62%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let counter=init
+    return {
+        increment:function(){
+            return counter+=1
+        },
+        decrement:function(){
+            return counter-=1
+        },
+        reset:function(){
+            return counter=init
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
